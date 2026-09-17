e7b4d94c75e2cf474c8106644c11b7d75d234e14
pawan kanta sharma <pawankantasharma21@gmail.com>
2026-09-17 16:49:41 -0700

    files

---
+++ 1.js
@@ -0,0 +1,7 @@
+let n = 20
+if (n%2==0) {
+    console.log("even")
+}
+else {
+    console.log("odd")
+} 
--- oddeven.js
+++ oddeven.js
@@ -1,7 +1,1 @@
-let x=3
-if(x%2==0){
-    console.log("even")
-}   
-else{
-    console.log("odd")
-}
+let nums1 = 20
