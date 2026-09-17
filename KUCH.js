65fa85fa55a8e1ea947611fc07563d3a0ec7db99
pawan kanta sharma <pawankantasharma21@gmail.com>
2026-09-17 13:23:30 -0700

    odd even

--- KUCH.js
+++ KUCH.js
@@ -1,1 +1,7 @@
 
+const length = 10;
+const width = 5;
+
+const area = length * width;
+
+console.log("Area of rectangle:", area);
---
+++ oddeven.js
@@ -0,0 +1,7 @@
+let x=3
+if(x%2==0){
+    console.log("even")
+}   
+else{
+    console.log("odd")
+}
